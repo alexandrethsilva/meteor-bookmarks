c0660935355c12be3681da9e71b851b73be0ebc6
Alexandre Theodoro da Silva <alexandrethsilva@mac.com>
2014-04-26 19:45:20 +0200

    Fixes collapsed layout

--- app/client/views/application/layout.js
+++ app/client/views/application/layout.js
@@ -1,3 +1,11 @@
 Template.layout.helpers({
   viewTitle: function() { return Session.get('viewTitle'); }
 });
+
+// Spinner package overwrite, to enable multiple spinners on a single document
+Template.spinner.rendered = function(){
+  var targets = [].slice.call( document.querySelectorAll('.spinner') );
+  targets.forEach( function( target ){
+    var spinner =  new Spinner( Meteor.Spinner.options ).spin( target );
+  } );
+};
